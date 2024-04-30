7823c548c7c9fa3f0ea778622de6f49dce76c99c
ibrahimcanerdogan <i.erdogan@ebebek.com>
2024-04-30 22:57:42 +0300

    Crypto App finished.

---
+++ Crypto App/app/src/main/java/com/ibrahimcanerdogan/cryptoapp/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.ibrahimcanerdogan.cryptoapp.util
+
+object Constants {
+    const val BASE_URL = "https://pro-api.coinmarketcap.com/"
+    const val LIMIT = "40"
+    const val API_KEY = "YOUR_COIN_MARKET_API_KEY"
+}
